8b262bd015ddcf7fe9daa946cd5b4650d2394535
CedszxcJulian <40488618+CedszxcJulian@users.noreply.github.com>
2020-05-05 16:14:10 +0800

    added updates

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -14,15 +14,15 @@ const users = [
     caption: "User1",
     // You will need to prepend the image path with your baseUrl
     // if it is not '/', like: '/test-site/img/image.jpg'.
-    image: "/img/undraw_open_source.svg",
+    image: "/img/2020-03-24.png",
     infoLink: "https://www.facebook.com",
     pinned: true,
   },
 ];
 
 const siteConfig = {
-  title: "Test Site", // Title for your website.
-  tagline: "A website for testing",
+  title: "IA Innovations EKYC", // Title for your website.
+  tagline: "EKYC Service Documentation",
   url: "https://your-docusaurus-test-site.com", // Your website URL
   baseUrl: "/", // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
@@ -48,9 +48,9 @@ const siteConfig = {
   users,
 
   /* path to images for header/footer */
-  headerIcon: "img/favicon.ico",
-  footerIcon: "img/favicon.ico",
-  favicon: "img/favicon.ico",
+  headerIcon: "img/2020-03-24.png",
+  footerIcon: "img/2020-03-24.png",
+  favicon: "img/2020-03-24.png",
 
   /* Colors for website */
   colors: {
@@ -105,6 +105,11 @@ const siteConfig = {
   // You may provide arbitrary config keys to be used as needed by your
   // template. For example, if you need your repo's URL...
   //   repoUrl: 'https://github.com/facebook/test-site',
+
+  url: "https://ia-cedrick.github.io", // Replace USERNAME with your GitHub username.
+  baseUrl: "/rek-service-documentation/", // The name of your GitHub project.
+  projectName: "rek-service-documentation", // The name of your GitHub project. Same as above.
+  organizationName: "ia-cedrick", // Your GitHub username.
 };
 
 module.exports = siteConfig;
